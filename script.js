8002116f6528023ed33a91a4e1dffc92d5f1dcea
Djoseffer Domingues <136139177+Djoseffer@users.noreply.github.com>
2023-07-26 17:02:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.getElementById("loginForm").addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the default form submission behavior
+    event.preventDefault(); 
 
-    // Get the values entered by the user
+    
     var username = document.getElementById("username").value; true
     var password = document.getElementById("password").value; true
 
-    // Perform actions with the entered data (e.g., validation, API calls, etc.)
+    
     if (username === "barbie" && password === "1234") {
         alert("Welcome, Barbie!");
-        // Add here the code to redirect the user to another page after successful login.
+        
         window.location.href = "https://shop.mattel.com/pt-br/pages/barbie";
     } else {
         alert("Invalid username or password. Please try again.");
